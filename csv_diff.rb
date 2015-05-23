75612106bb90b0dbed9ce0135f45e50bcefef859
Kawai Toshikazu <toshi@Kawai-no-MacBook-Air.local>
2015-05-23 19:17:28 +0900

    INSERTED, DELETEDを+,-に変更

--- csv_diff.rb
+++ csv_diff.rb
@@ -41,9 +41,9 @@ File.open(DIFF_REF, 'r') {|f|
 
 puts "from #{DIFF_ORG} to #{DIFF_REF}"
 (org_keys - ref_keys).each {|k|
-  print "DELETED, " + org[k]
+  print "-, " + org[k]
 }
 
 (ref_keys - org_keys).each {|k|
-    print "INSERTED, " + ref[k]
+    print "+, " + ref[k]
 }
